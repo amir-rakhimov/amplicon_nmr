7502fef8d0842666734621b8c2742ac2971594e5
amir-rakhimov <amir.rakhimov.b@gmail.com>
2024-04-24 14:51:34 +0900

    added comments to the pooled-job script

--- code/bash-scripts/pooled-job-single.sh
+++ code/bash-scripts/pooled-job-single.sh
@@ -1,27 +1,50 @@
 #!/usr/bin/env bash
+# 1. Set up the variables
+### Truncation length for DADA2
 trunc_len=234
+### Front and reverse primers
 front_primer=CCTACGGGNGGCWGCAG
 rev_primer=GACTACHVGGGTATCTAATCC
+### Position of the front primer
 front_primer_position=341f
+### metadata file contains the fastq file paths
 metadata_file="filenames-single-pooled-raw-supercomp.tsv"
+### Truncation length for the taxonomic reference database
 ref_db_trunc_len=234
 conda activate qiime2
+# 2 Import the fastq files using the metadata file.
+### We need to specify the input format. Since we're using only
+### front reads (single reads), we need to specify SingleEndFastqManifestPhred33V2
 qiime tools import \
   --type  'SampleData[SequencesWithQuality]' \
   --input-path ${metadata_file} \
   --output-path pooled-single-end-demux.qza \
   --input-format SingleEndFastqManifestPhred33V2 
+### The command below will create a visualization file for quality check.
+### Normally, you should open the file and decide the truncation length. 
+### But I have already explored the final output with different truncation lentgths,
+### so it's already set at the beginning of this script. 
 qiime demux summarize \
   --i-data pooled-single-end-demux.qza \
   --o-visualization pooled-single-end-demux.qzv
+# 3. Remove the sequencing adapters with cutadapt.
+### Specify the primer sequences. We're using only front reads, so we specify
+### only --p-front
 qiime cutadapt trim-single \
   --i-demultiplexed-sequences pooled-single-end-demux.qza \
   --p-cores 8 \
   --p-front ${front_primer} \
   --o-trimmed-sequences pooled-trimmed-single-end.qza
+### Create a visualization to see that primers were removed: the sequences will get shorter
 qiime demux summarize \
   --i-data pooled-trimmed-single-end.qza \
   --o-visualization pooled-trimmed-single-end.qzv
+# 4. Denoise, dereplicate, and filter reads with DADA2.
+### --denoise-single means we use single-end sequences
+### --p-trim-left trims 5prime end 
+### --p-trunc-len position at which sequences should be truncated. 
+### It will truncate the 3prime end. Reads that are shorter than
+### p-trunc-len will be discarded.
 qiime dada2 denoise-single \
   --i-demultiplexed-seqs pooled-trimmed-single-end.qza \
   --p-trim-left 0 \
@@ -29,13 +52,16 @@ qiime dada2 denoise-single \
   --o-table pooled-single-table-trimmed-dada2-${trunc_len}.qza \
   --o-representative-sequences pooled-single-rep-seqs-trimmed-dada2-${trunc_len}.qza \
   --o-denoising-stats pooled-single-stats-trimmed-dada2-${trunc_len}.qza
+### Visualise the denoising-stats file
 qiime metadata tabulate \
   --m-input-file pooled-single-stats-trimmed-dada2-${trunc_len}.qza \
   --o-visualization pooled-single-stats-trimmed-dada2-${trunc_len}.qzv
+### Visualise the feature table. Use the metadata file to check library size by metadata
 qiime feature-table summarize \
   --i-table pooled-single-table-trimmed-dada2-${trunc_len}.qza \
   --m-sample-metadata-file ${metadata_file} \
   --o-visualization pooled-single-table-trimmed-dada2-${trunc_len}.qzv
+### Visualise representative-sequences. This will show you sequence lengths
 qiime feature-table tabulate-seqs \
   --i-data pooled-single-rep-seqs-trimmed-dada2-${trunc_len}.qza \
   --o-visualization pooled-single-rep-seqs-trimmed-dada2-${trunc_len}.qzv
