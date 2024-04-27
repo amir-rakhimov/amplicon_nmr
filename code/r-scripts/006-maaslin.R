1abbba10279e6f1051040a57ef5c36b0577944cb
amir-rakhimov <amir.rakhimov.b@gmail.com>
2024-04-27 16:51:33 +0900

    changed the input and output directories. Now dates are used in the output file names

--- code/r-scripts/006-maaslin.R
+++ code/r-scripts/006-maaslin.R
@@ -1,15 +1,23 @@
-library(tidyverse)
+# if(!requireNamespace("BiocManager")){
+#   install.packages("BiocManager")
+# }
+# BiocManager::install(c("phyloseq","Maaslin2"))
+# install.packages(c("tidyverse","Polychrome"))
 library(phyloseq)
+library(tidyverse)
 library(Maaslin2)
 library(vegan)
-authorname<-"merged"
+authorname<-"pooled"
 truncationlvl<-"234"
 agglom.rank<-"Genus"
 read.end.type<-"single"
+date_time<-"20240426_21_44_30"
 
-load(paste0("./rdafiles/",paste(authorname,read.end.type,"qiime2",
-                                truncationlvl,agglom.rank,
-                                "phyloseq-workspace.RData",sep = "-")))
+load(file.path("./output/rdafiles",paste(
+  date_time,
+  authorname,read.end.type,"qiime2",
+  truncationlvl,agglom.rank,
+  "phyloseq-workspace.RData",sep = "-")))
 
 custom.levels<-c("NMR",
                  "B6mouse",
@@ -19,32 +27,34 @@ custom.levels<-c("NMR",
                  "hare",
                  "rabbit",
                  "spalax",
-                 "pvo",
-                 "NMRwt"
+                 "pvo"
+                 # ,
+                 # "NMRwt"
                  )
-ref.level<-"DMR"
+ref.level<-"NMR"
 # Import data ####
 rare.status<-"rare"
 filter.status<-"nonfiltered"
-ps.q.df.maaslin.input<-read.table(paste0("./rtables/",authorname,"/ps.q.df.",
-                                         rare.status,".",filter.status,"-",agglom.rank,"-",
-                                         paste(custom.levels,collapse = '-'),".tsv"),
-                                  header = T)
+ps.q.df.maaslin.input<-read.table(
+  file.path("./output/rtables",authorname,paste0(
+    paste(
+      "20240426_22_00_04",
+      "ps.q.df.rare-nonfiltered",agglom.rank,
+      paste(custom.levels,collapse = '-'),sep = "-"),
+    ".tsv")),
+  header = T)
+
 ps.q.agg<-ps.q.agg%>%
   filter(class%in%custom.levels,Abundance!=0)
 custom.md<-custom.md%>%
-  filter(class%in%custom.levels)
-ps.q.total<-ps.q.total%>%
-  filter(Sample%in%rownames(custom.md))
-ps.q.1pc<-ps.q.1pc%>%
   filter(class%in%custom.levels)
 # Maaslin2 ####
 # Input data and metadata ####
 # convert the data frames into wide format
 ps.q.df.maaslin.input.wide<-ps.q.df.maaslin.input%>%
   filter(class%in%custom.levels,Abundance!=0)%>%
-  dplyr::select(Sample,Abundance,Taxon)%>%
-  pivot_wider(names_from = "Taxon", # or OTU
+  dplyr::select(Sample,Abundance,all_of(agglom.rank))%>%
+  pivot_wider(names_from = all_of(agglom.rank),
               values_from = "Abundance",
               values_fill = 0)%>%
   as.data.frame()
@@ -59,20 +69,24 @@ maaslin.fit_data =
   Maaslin2(input_data = ps.q.df.maaslin.input.wide, 
            input_metadata = custom.md, 
            min_prevalence = 0,
-           normalization = "TSS",
-           transform = "AST",
            analysis_method = "LM",
+           normalization = "TSS",
+           transform = "LOG",
            random_effects = NULL,
            standardize = FALSE,
-           output = paste0("./output/maaslin2/",authorname,"-output/",
-                           rare.status,"/",paste(paste(custom.levels,collapse = '-'),
+           output = file.path("./output/maaslin2",paste0(authorname,"-output"),
+                           rare.status,paste(
+                             paste(format(Sys.time(),format="%Y%m%d"),
+                                   format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+                             paste(custom.levels,collapse = '-'),
                            truncationlvl,"ref",ref.level,sep = "-")), 
            fixed_effects = c("class"),
            reference = paste0("class,",ref.level),
            max_significance = 0.05)
-
-save.image(paste0("./rdafiles/",
-                  paste("maaslin",rare.status,filter.status,agglom.rank,
+save.image(file.path("./output/rdafiles",paste(
+  paste(format(Sys.time(),format="%Y%m%d"),
+        format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+  "maaslin",rare.status,filter.status,agglom.rank,
                         paste(custom.levels,collapse = '-'),
                         truncationlvl,ref.level,"workspace.RData",sep="-")))
 q()
