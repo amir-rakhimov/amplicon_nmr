7045758b5900d37e639199207c2f485355deb40a
amir-rakhimov <rakhimovamir2001@gmail.com>
2024-05-28 14:19:10 +0900

    removed website

--- _site/code/r-scripts/make_features_pretty.R
+++
@@ -1,9 +0,0 @@
-make_features_pretty<-function(features.list, featurecolumn){
-  features.list[[featurecolumn]]<-gsub("\\.\\."," (",features.list[[featurecolumn]])
-  features.list[[featurecolumn]]<-gsub("\\.$",")",features.list[[featurecolumn]])
-  features.list[[featurecolumn]]<-gsub("X\\.","[",features.list[[featurecolumn]])
-  features.list[[featurecolumn]]<-gsub("\\._","]_",features.list[[featurecolumn]])
-  features.list[[featurecolumn]]<-gsub("\\(\\.","([",features.list[[featurecolumn]])
-  features.list[[featurecolumn]]<-gsub("\\.","-",features.list[[featurecolumn]])
-  return(features.list)
-}
