573590eec45709d05aa9e0d002e34516e3f92d38
amir-rakhimov <rakhimovamir2001@gmail.com>
2024-06-28 12:17:49 +0900

    combine visualisation with downstream processing

--- code/r-scripts/010-diff-abund-downstream.R
+++ code/r-scripts/010-diff-abund-downstream.R
@@ -1,14 +1,15 @@
 library(tidyverse)
-library(phyloseq)
-library(Maaslin2)
-library(vegan)
-library(ALDEx2)
+# library(phyloseq)
+# library(Maaslin2)
+# library(vegan)
+# library(ALDEx2)
 # Import data ####
 ref.level="NMR"
 truncationlvl<-"234"
 agglom.rank<-"Genus"
 source("./code/r-scripts/make_features_maaslin.R")
 phyloseq.workspace.date_time<-"20240426_21_44_30"
+# Tables of differentially abundant taxa
 maaslin.date_time<-"20240427_16_22_09"
 aldex2.date_time<-"20240427_17_39_41"
 ancombc.date_time<-"20240427_17_12_27"
@@ -42,7 +43,7 @@ custom.md<-custom.md%>%
 
 maaslin.signif.features<-
   read.table(file.path("./output/rtables",authorname,
-                        paste(maaslin.date_time,"maaslin",rare.status,
+                        paste(maaslin.date_time,"maaslin2",rare.status,
                               filter.status,agglom.rank,
                               paste(custom.levels,collapse = '-'),
                               truncationlvl,ref.level,
@@ -80,35 +81,35 @@ aldex.neg.effect<-aldex.signif.features%>%
 ancombc.signif.decreased<-subset(ancombc.signif.features,
                        rowSums(ancombc.signif.features[,-c(1,2)]<0)==ncol(ancombc.signif.features[,-c(1,2)]))
 
-write.table(maaslin.signif.decreased,
-            file=file.path("./output/rtables",authorname,
-                           paste(paste(format(Sys.time(),format="%Y%m%d"),
-                                       format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
-                                 "maaslin.signif.decreased",rare.status,
-                                 filter.status,agglom.rank,
-                                 paste(custom.levels,collapse = '-'),truncationlvl,
-                                 ref.level,"signif.tsv",sep="-")),
-            row.names = F,sep = "\t")
-
-write.table(aldex.neg.effect,
-            file=file.path("./output/rtables",authorname,
-                           paste(paste(format(Sys.time(),format="%Y%m%d"),
-                                       format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
-                                 "aldex.neg.effect",rare.status,
-                                 filter.status,agglom.rank,
-                                 paste(custom.levels,collapse = '-'),truncationlvl,
-                                 ref.level,"signif.tsv",sep="-")),
-            row.names = F,sep = "\t")
-
-write.table(ancombc.signif.decreased,
-            file=file.path("./output/rtables",authorname,
-                           paste(paste(format(Sys.time(),format="%Y%m%d"),
-                                       format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
-                                 "ancombc.signif.decreased",rare.status,
-                                 filter.status,agglom.rank,
-                                 paste(custom.levels,collapse = '-'),truncationlvl,
-                                 ref.level,"signif.tsv",sep="-")),
-            row.names = F,sep = "\t")
+# write.table(maaslin.signif.decreased,
+#             file=file.path("./output/rtables",authorname,
+#                            paste(paste(format(Sys.time(),format="%Y%m%d"),
+#                                        format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+#                                  "maaslin.signif.decreased",rare.status,
+#                                  filter.status,agglom.rank,
+#                                  paste(custom.levels,collapse = '-'),truncationlvl,
+#                                  ref.level,"signif.tsv",sep="-")),
+#             row.names = F,sep = "\t")
+# 
+# write.table(aldex.neg.effect,
+#             file=file.path("./output/rtables",authorname,
+#                            paste(paste(format(Sys.time(),format="%Y%m%d"),
+#                                        format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+#                                  "aldex.neg.effect",rare.status,
+#                                  filter.status,agglom.rank,
+#                                  paste(custom.levels,collapse = '-'),truncationlvl,
+#                                  ref.level,"signif.tsv",sep="-")),
+#             row.names = F,sep = "\t")
+# 
+# write.table(ancombc.signif.decreased,
+#             file=file.path("./output/rtables",authorname,
+#                            paste(paste(format(Sys.time(),format="%Y%m%d"),
+#                                        format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+#                                  "ancombc.signif.decreased",rare.status,
+#                                  filter.status,agglom.rank,
+#                                  paste(custom.levels,collapse = '-'),truncationlvl,
+#                                  ref.level,"signif.tsv",sep="-")),
+#             row.names = F,sep = "\t")
 
 # find common significant features between three tools
 Reduce(intersect,list(maaslin.signif.features$feature,aldex.signif.features$Taxon,
@@ -128,62 +129,83 @@ common.signif<-Reduce(intersect,list(maaslin.signif.features$feature,aldex.signi
 common.decreased<-Reduce(intersect,list(maaslin.signif.decreased$feature,
                                      ancombc.signif.decreased$taxon_id))
 
-write.table(common.decreased,
-            file=file.path("./output/rtables",authorname,
-                           paste(paste(format(Sys.time(),format="%Y%m%d"),
-                                       format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
-                                 "significant-features",rare.status,
-                                 filter.status,agglom.rank,
-                                 paste(custom.levels,collapse = '-'),truncationlvl,
-                                 ref.level,"signif.tsv",sep="-")),
-            row.names = F,sep = "\t",col.names = F)
+# write.table(common.decreased,
+#             file=file.path("./output/rtables",authorname,
+#                            paste(paste(format(Sys.time(),format="%Y%m%d"),
+#                                        format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+#                                  "significant-features",rare.status,
+#                                  filter.status,agglom.rank,
+#                                  paste(custom.levels,collapse = '-'),truncationlvl,
+#                                  ref.level,"signif.tsv",sep="-")),
+#             row.names = F,sep = "\t",col.names = F)
 
 
-maaslin.signif.features<-c("Desulfovibrio","Campylobacter")
-for (i in seq(maaslin.signif.features)){
-  taxon.plot<-ps.q.agg%>%
-    filter(class=="NMR")%>%
-    left_join(custom.md,by="Sample")%>%
-    filter(Genus==maaslin.signif.features[i])%>%
-    group_by(Sample,old_agegroup,Genus)%>%
-    summarise(RelativeAbundance=sum(RelativeAbundance))%>%
-    ggplot(aes(x=Sample,y=RelativeAbundance))+
-    geom_bar(stat="identity")+
-    ylab("Relative Abundance (%)")+
-    
-    facet_grid(~old_agegroup,scales = "free_x")+
-    ggtitle(paste(maaslin.signif.features[i],"relative abundance"))+
-    theme_bw()+
-    theme(axis.line = element_blank(),
-          strip.text.x = ggtext::element_markdown(size = 20),# the name of
-          # each facet will be recognised as a markdown object, so we can
-          # add line breaks (cause host names are too long)
-          axis.text.x = element_text(size=20),# rotate
-          # the x-axis labels by 45 degrees and shift to the right
-          axis.text.y = element_text(size=20), # size of y axis ticks
-          axis.title = element_text(size = 20), # size of axis names
-          plot.title = ggtext::element_markdown(size = 25), # the plot
-          # title will be recognised as a markdown object, so we can
-          # add line breaks (cause host names are too long)
-          plot.caption = element_text(size=23),# size of plot caption
-          legend.text = element_text(size = 20),# size of legend text
-          legend.title = element_text(size = 25), # size of legend title
-          legend.position = "right")
-  ggsave(paste0("./images/barplots/",
-                paste(paste(format(Sys.time(),format="%Y%m%d"),
-                            format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
-                      "barplot","NMR",maaslin.signif.features[i],
-                      agglom.rank,sep = "-"),".png"),
-         plot=taxon.plot,
-         width = 6000,height = 3000,
-         units = "px",dpi=300,device = "png")
-  ggsave(paste0("./images/barplots/",
-                paste(paste(format(Sys.time(),format="%Y%m%d"),
-                            format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
-                      "barplot","NMR",maaslin.signif.features[i],
-                      agglom.rank,sep = "-"),".tiff"),
-         plot=taxon.plot,
-         width = 6000,height = 3000,
-         units = "px",dpi=300,device = "tiff")
-}
 
+####
+library(Polychrome)
+library(ggtext)
+pretty.level.names<-c("NMR" = "*Heterocephalus glaber*", # better labels for facets
+                      "B6mouse" = "B6 mouse",
+                      "MSMmouse" = "MSM/Ms mouse",
+                      "FVBNmouse" = "FVB/N mouse",
+                      "DMR" = "*Fukomys Damarensis*",
+                      "hare" = "*Lepus europaeus*",
+                      "rabbit" = "*Oryctolagus cuniculus*",
+                      "spalax" = "*Nannospalax leucodon*",
+                      "pvo" = "*Pteromys volans orii*"#,
+                      # "NMRwt"="Wild *Heterocephalus glaber*"
+)
+set.seed(1)
+custom.fill<-createPalette(length(custom.levels),
+                           seedcolors = c("#EE2C2C","#5CACEE","#00CD66",
+                                          "#FF8C00","#BF3EFF", "#00FFFF",
+                                          "#FF6EB4","#00EE00","#EEC900",
+                                          "#FFA07A"))
+names(custom.fill)<-custom.levels
+swatch(custom.fill)
+
+ps.q.agg%>%
+  filter(get(agglom.rank)%in%common.decreased)%>%
+  group_by_at(c("class",agglom.rank))%>%
+  ggplot(aes(x=factor(class,level=rev(custom.levels)),
+             y=RelativeAbundance,
+             fill=factor(class)))+
+  geom_boxplot(show.legend = FALSE)+
+  # facet_wrap(~Genus,scales = "free_x",
+  #            ncol = 2)+
+  theme_bw()+
+  coord_flip()+
+  labs(x="",
+       y="Relative abundance (%)")+
+  scale_color_manual(breaks = rev(unname(pretty.level.names)),
+                     labels=rev(unname(pretty.level.names)))+
+  scale_x_discrete(labels=rev(pretty.level.names),
+                   limits=rev(custom.levels))+ # rename boxplot labels (x axis)
+  scale_fill_manual(values = rev(custom.fill))+
+  theme(plot.margin=unit(c(1,1,1,2), 'cm'),
+        axis.title.y = element_blank(),
+        axis.title = element_text(size = 20),
+        axis.text.y = ggtext::element_markdown(size=18),
+        axis.text.x = element_text(size=20),
+        strip.text.x = element_text(size=20),
+        plot.title = element_text(size = 27),
+        legend.text = element_text(size = 20),
+        legend.title = element_text(size = 25),
+        legend.position = "none")+
+  ggtitle(paste0("Relative abundance of naked mole-rat-specific taxa"))
+ggsave(paste0("./images/taxaboxplots/",
+              paste(paste(format(Sys.time(),format="%Y%m%d"),
+                          format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+                    "NMR-specific-bacteria",
+                    sep = "-"),".","png"),
+       plot=last_plot(),
+       width = 4000,height = 12000,
+       units = "px",dpi=300,device = "png")
+ggsave(paste0("./images/taxaboxplots/",
+              paste(paste(format(Sys.time(),format="%Y%m%d"),
+                          format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
+                    "NMR-specific-bacteria",
+                    sep = "-"),".","tiff"),
+       plot=last_plot(),
+       width = 4000,height = 12000,
+       units = "px",dpi=300,device = "tiff")
