d890bbe61ff70d9c014fd687a8cc08675f40bf28
amir-rakhimov <rakhimovamir2001@gmail.com>
2024-06-28 12:06:48 +0900

    added code from other scripts

---
+++ code/bash-scripts/aldex-job.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash                                                                                                     
+R-4.3.0/bin/Rscript alldir/aldex-script-mice-age.R
+# R-4.3.0/bin/Rscript alldir/aldex-script-mice-strain.R
+# R-4.3.0/bin/Rscript alldir/aldex-script-nmr-age.R
+# R-4.3.0/bin/Rscript alldir/aldex-script-complex.R
