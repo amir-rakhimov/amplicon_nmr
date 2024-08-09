aeec82621aadbd42dde2d8249045d61a85cd7d8a
amir-rakhimov <rakhimovamir2001@gmail.com>
2024-08-09 11:08:14 +0900

    added comments with explanation of commands, added more gsub commands

--- code/r-scripts/make_features_maaslin.R
+++ code/r-scripts/make_features_maaslin.R
@@ -1,13 +1,36 @@
 make_features_maaslin<-function(features.list, featurecolumn){
-  features.list[[featurecolumn]]<-gsub(" \\(","\\.\\.",features.list[[featurecolumn]])
+  features.list[[featurecolumn]]<-gsub("\\(","\\.",features.list[[featurecolumn]])
+  features.list[[featurecolumn]]<-gsub("\\)","\\.",features.list[[featurecolumn]])
+  features.list[[featurecolumn]]<-gsub("\\:","\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub("\\)$","\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub("^\\[","X\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub("^(\\d)", "X\\1", features.list[[featurecolumn]])
-  features.list[[featurecolumn]]<-gsub("\\] ","\\.\\.",features.list[[featurecolumn]])
+  features.list[[featurecolumn]]<-gsub("\\]","\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub("\\(\\[","\\(\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub("\\[","\\.",features.list[[featurecolumn]])
+  features.list[[featurecolumn]]<-gsub("\\]$","\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub("-","\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub("\\+","\\.",features.list[[featurecolumn]])
   features.list[[featurecolumn]]<-gsub(" ","\\.",features.list[[featurecolumn]])
   return(features.list)
 }
+#original DIHYDRODIPICSYN-RXN: (expasy) 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7]
+# 1. DIHYDRODIPICSYN-RXN: .expasy) 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7]
+# 2. DIHYDRODIPICSYN-RXN: .expasy. 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7]
+# 3. DIHYDRODIPICSYN-RXN. .expasy. 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7]
+# 4. DIHYDRODIPICSYN-RXN. .expasy. 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7] 
+#nothing here but end of the line ) will become .
+# 5. DIHYDRODIPICSYN-RXN. .expasy .4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7] 
+# nothing here but start of the line [ will become X
+# 6. DIHYDRODIPICSYN-RXN. .expasy. 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7] 
+# I forgot
+# 7. DIHYDRODIPICSYN-RXN. .expasy. 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7] 
+# nothing here but ] with space becomes ..
+# 8. DIHYDRODIPICSYN-RXN. .expasy. 4-hydroxy-tetrahydrodipicolinate synthase [4.3.3.7] 
+# nothing here but ([ becomes ..
+# 9. DIHYDRODIPICSYN-RXN. .expasy. 4-hydroxy-tetrahydrodipicolinate synthase .4.3.3.7] 
+# 10. DIHYDRODIPICSYN-RXN. .expasy. 4-hydroxy-tetrahydrodipicolinate synthase .4.3.3.7.
+# 11. DIHYDRODIPICSYN.RXN. .expasy. 4.hydroxy.tetrahydrodipicolinate synthase .4.3.3.7. 
+# 12. DIHYDRODIPICSYN.RXN. .expasy. 4.hydroxy.tetrahydrodipicolinate synthase .4.3.3.7. 
+# nothing here but + becomes .
+# 13. DIHYDRODIPICSYN.RXN...expasy..4.hydroxy.tetrahydrodipicolinate.synthase..4.3.3.7. 
