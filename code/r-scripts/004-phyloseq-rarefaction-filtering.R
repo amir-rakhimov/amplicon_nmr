1ca4f012c8408d200958814bf1ccb10948df6cc9
amir-rakhimov <rakhimovamir2001@gmail.com>
2024-08-14 13:59:34 +0900

    added comments at the beginning of the script

--- code/r-scripts/004-phyloseq-rarefaction-filtering.R
+++ code/r-scripts/004-phyloseq-rarefaction-filtering.R
@@ -1,3 +1,11 @@
+# Rarefaction and filtering ####
+# This script uses the dataframe from the 001-phyloseq-qiime2.R script, and 
+# rarefies it by the smallest sample size. If you are working with a 
+# subset of hosts (e.g. NMR), you can filter the dataset to include only 
+# the subset of samples.
+# You can plot a rarefaction curve and filter the dataset, 
+# if you need. But we only do rarefaction.
+
 # if(!requireNamespace("BiocManager")){
 #   install.packages("BiocManager")
 # }
@@ -34,21 +42,18 @@ pretty.facet.labels<-c("NMR" = "*Heterocephalus glaber*", # better labels for fa
 )
 custom.levels<-intersect(names(pretty.facet.labels),custom.md$class)
 
-# If we're working with NMR only
+# If we're working with NMR only or mice
 # custom.levels<-c("NMR")
 # custom.levels<-c("B6mouse",
 #                  "MSMmouse",
 #                  "FVBNmouse")
 
 # Import data ####
+# If you need only NMR samples, filter the dataset
 ps.q.df <-ps.q.agg%>%
-    # filter(class=="NMR")%>%
+  filter(class %in% custom.levels,Abundance!=0)%>%
   select(Sample,Abundance,class,all_of(agglom.rank))%>%
   filter(Abundance!=0)
-
-ps.q.df<-ps.q.df%>%
-  filter(class %in% custom.levels,Abundance!=0)
-
 
 # Convert the data frame into wide format: rows are samples and columns
 # are taxa
@@ -66,7 +71,6 @@ ps.q.df.wide<-ps.q.df.wide[,-c(1,2)]
 # Rarefaction ####
 # find the smallest sample size
 min.n_seqs.all<-ps.q.df%>%
-  # filter(class=="NMR")%>%
   filter(class %in% custom.levels)%>%
   select(Sample,all_of(agglom.rank),Abundance)%>%
   group_by(Sample)%>%
@@ -143,53 +147,53 @@ write.table(ps.q.df.rare,
             sep = "\t")
 
 
-# Filtering by prevalence ####
-# for each host, calculate the number of samples an ASV was observed in 
-# so the same ASV can be observed in multiple hosts.
-# observed_samples shows number of samples per host
-ps.q.sample_counts <- ps.q.agg%>%
-  select(OTU,Sample,class,Abundance,Taxon,Genus,Family)%>%
-  filter(Abundance!=0)%>%
-  group_by(class,Family,Genus) %>% 
-  summarize(observed_samples = n_distinct(Sample))
-
-# calculate the total number of samples for each host
-ps.q.host_counts <- ps.q.agg%>%
-  select(OTU,Sample,class,Abundance,Taxon,Genus,Family)%>%
-  filter(Abundance!=0)%>%
-  group_by(class)%>%
-  summarize(total_samples = n_distinct(Sample)) %>%
-  ungroup()
-
-# Join host counts with sample counts
-# Create the PercentageSamples column: num of samples  from a specific host
-# an ASV was observed in divided by the total number of samples in that host
-# Then, we join our dataframe with mean relative abundance df (ps.agg.rel.pooled).
-# Then, we add the Taxon column from ps.q.agg.abs
-ps.q.prevalences <- ps.q.host_counts %>%
-  left_join(ps.q.sample_counts, by = "class") %>%
-  mutate(PercentageSamples = observed_samples/total_samples*100)%>% # percentage
-  # of samples an ASV was observed in
-  left_join(ps.q.agg[,c("class","Genus","Family","Taxon","MeanRelativeAbundance")],
-            by=c("class","Genus","Family")) # add mean
-  # relative abundances and the Taxon column
-
-## filter by percentage of samples an ASV was observed in ####
-ps.q.df.norare.filtered <-ps.q.df%>%
-  left_join(ps.q.prevalences,by=c("class","Taxon"),relationship = "many-to-many")%>%
-  filter(PercentageSamples>=10)
-ps.q.df.rare.filtered<-ps.q.df.rare%>%
-  left_join(ps.q.prevalences,by=c("class","Taxon"),relationship = "many-to-many")%>%
-  filter(PercentageSamples>=10,MeanRelativeAbundance>=1)
-
-
-write.table(ps.q.df.norare.filtered,
-            file = paste0("./rtables/",authorname,"/ps.q.df.norare.filtered-",
-                          paste(agglom.rank,custom.levels,collapse = '-'),".tsv"),
-            row.names = F,
-            sep = "\t")
-write.table(ps.q.df.rare.filtered,
-            file = paste0("./rtables/",authorname,"/ps.q.df.rare.filtered-",
-                          paste(agglom.rank,custom.levels,collapse = '-'),".tsv"),
-            row.names = F,
-            sep = "\t")
+# # Filtering by prevalence ####
+# # for each host, calculate the number of samples an ASV was observed in 
+# # so the same ASV can be observed in multiple hosts.
+# # observed_samples shows number of samples per host
+# ps.q.sample_counts <- ps.q.agg%>%
+#   select(OTU,Sample,class,Abundance,Taxon,Genus,Family)%>%
+#   filter(Abundance!=0)%>%
+#   group_by(class,Family,Genus) %>% 
+#   summarize(observed_samples = n_distinct(Sample))
+# 
+# # calculate the total number of samples for each host
+# ps.q.host_counts <- ps.q.agg%>%
+#   select(OTU,Sample,class,Abundance,Taxon,Genus,Family)%>%
+#   filter(Abundance!=0)%>%
+#   group_by(class)%>%
+#   summarize(total_samples = n_distinct(Sample)) %>%
+#   ungroup()
+# 
+# # Join host counts with sample counts
+# # Create the PercentageSamples column: num of samples  from a specific host
+# # an ASV was observed in divided by the total number of samples in that host
+# # Then, we join our dataframe with mean relative abundance df (ps.agg.rel.pooled).
+# # Then, we add the Taxon column from ps.q.agg.abs
+# ps.q.prevalences <- ps.q.host_counts %>%
+#   left_join(ps.q.sample_counts, by = "class") %>%
+#   mutate(PercentageSamples = observed_samples/total_samples*100)%>% # percentage
+#   # of samples an ASV was observed in
+#   left_join(ps.q.agg[,c("class","Genus","Family","Taxon","MeanRelativeAbundance")],
+#             by=c("class","Genus","Family")) # add mean
+#   # relative abundances and the Taxon column
+# 
+# ## filter by percentage of samples an ASV was observed in ####
+# ps.q.df.norare.filtered <-ps.q.df%>%
+#   left_join(ps.q.prevalences,by=c("class","Taxon"),relationship = "many-to-many")%>%
+#   filter(PercentageSamples>=10)
+# ps.q.df.rare.filtered<-ps.q.df.rare%>%
+#   left_join(ps.q.prevalences,by=c("class","Taxon"),relationship = "many-to-many")%>%
+#   filter(PercentageSamples>=10,MeanRelativeAbundance>=1)
+# 
+# 
+# write.table(ps.q.df.norare.filtered,
+#             file = paste0("./rtables/",authorname,"/ps.q.df.norare.filtered-",
+#                           paste(agglom.rank,custom.levels,collapse = '-'),".tsv"),
+#             row.names = F,
+#             sep = "\t")
+# write.table(ps.q.df.rare.filtered,
+#             file = paste0("./rtables/",authorname,"/ps.q.df.rare.filtered-",
+#                           paste(agglom.rank,custom.levels,collapse = '-'),".tsv"),
+#             row.names = F,
+#             sep = "\t")
