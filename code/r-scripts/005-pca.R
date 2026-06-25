ae0f0a02f110bf6a36e9b1d794f46543a10507d3
amir-rakhimov <amir.rakhimov.b@gmail.com>
2026-06-25 15:11:07 +0900

    Added rclr averaging (calculate RCLR on each rarefied subsample, then average all subsamples)

--- code/r-scripts/005-pca.R
+++ code/r-scripts/005-pca.R
@@ -29,6 +29,7 @@
 # }
 # BiocManager::install("phyloseq")
 # install.packages(c("tidyverse","vegan","Polychrome"))
+library(phyloseq)
 library(tidyverse)
 library(vegan) # Need v2.6-4 instead of current (2.7-1)
 #+ echo=FALSE
@@ -53,50 +54,100 @@ plot.types<-c("plot"="",
 
 #' The taxonomic rank that was used for agglomeration:
 agglom.rank<-"Genus"
-ps.q.agg<-readRDS(file=file.path(
-  community.composition.rdafiles,
-  paste("phyloseq-qiime",authorname,agglom.rank,read.end.type,
-        truncationlvl,"table.rds",sep = "-")))
+ps.q<-readRDS(file= ps.q.filtered.fname)
+
 custom.md <- readRDS(custom.md.path)
 custom.levels<-intersect(names(pretty.level.names),custom.md$class)
-#' Remove rows with 0 Abundance, if there's any.
-ps.q.agg<-ps.q.agg%>%
-    filter(class%in%custom.levels,Abundance!=0)
+
+ps.q.agg.asv.rare <-readRDS(file.path(community.composition.rdafiles,paste0(
+  paste("ps.q.rare","OTU",paste0(rare.num_samples,"_iter"),
+        paste(custom.levels,collapse = '-'),sep = "-"),".rds"))
+)
+
+ps.q.agg.agglom_rank.rare<-map(ps.q.agg.asv.rare, ~tax_glom(.x, taxrank = agglom.rank))
+
+ps.q.agg.agglom_rank.rare.melt <-map(ps.q.agg.agglom_rank.rare, ~psmelt(.x) %>% 
+                           filter(Abundance !=0) %>% 
+                             dplyr::select(Sample, Abundance, all_of(agglom.rank))%>%
+                             pivot_wider(names_from = all_of(agglom.rank),
+                                         values_from = Abundance,
+                                         values_fill = 0)%>%
+                             column_to_rownames("Sample"))
+
+
+rclr_list<-lapply(ps.q.agg.agglom_rank.rare.melt, function(wide.df){
+  # Samples are rows, taxa are columns
+  wide.df.rclr <-decostand(wide.df, method = "rclr", logbase = exp(1))
+  
+  missing.taxa <- setdiff(ps.q@tax_table%>%as.data.frame()%>%distinct(get(agglom.rank))%>%pull, 
+                          colnames(wide.df.rclr))
+  
+  missing.taxa.df <- matrix(nrow= nrow(wide.df.rclr),
+                            ncol = length(missing.taxa),
+                            data = 0,
+                            dimnames = list(rownames(wide.df.rclr),missing.taxa))
+  wide.df.rclr <- cbind(wide.df.rclr, missing.taxa.df)
+  wide.df.rclr <- wide.df.rclr%>%select(order(colnames(.)))
+  
+})
+
+rclr_avg <- Reduce ("+", rclr_list)/ length(rclr_list)
+prcomp.rclr_avg<- prcomp(rclr_avg, center = TRUE, scale. = FALSE)
+pca.q <-prcomp.rclr_avg
+
+#' #### Alternative: single rarefaction 
+#' ps.q.df.wide.pca <- ps.q.agg.agglom_rank.rare[[1]]
+#' ps.q.df.wide.pca.tfm <- rclr_list[[1]]
+#' #### OR raw????
+#' ps.q.df.pca.input <- ps.q.agg
+#' ####
+#' 
+#' if(agglom.rank=="OTU"){
+#'   ps.q.agg <- ps.q%>%
+#'     psmelt()
+#' }else{
+#'   ps.q.agg<-ps.q%>%
+#'     tax_glom(taxrank = agglom.rank)%>%
+#'     psmelt()%>%
+#'     select(-OTU)
+#' }
+#' #' Remove rows with 0 Abundance, if there's any.
+#' ps.q.agg<-ps.q.agg%>%
+#'     filter(class%in%custom.levels,Abundance!=0)
 #+ echo=FALSE
 ## 3. PCA ####
 #'
 #' ## PCA ####
 #' Import rarefied dataframe.
-ps.q.df.pca.input<-readRDS(
-  file.path(community.composition.rdafiles,
-            paste0(
-    paste("ps.q.df.rare-nonfiltered",agglom.rank,
-      paste(custom.levels,collapse = '-'),sep = "-"),
-    ".rds")))
-#' Convert into wide format.
-ps.q.df.wide.pca<-ps.q.df.pca.input%>%
-  dplyr::select(all_of(c(agglom.rank,"Sample","Abundance")))%>%
-  pivot_wider(names_from = all_of(agglom.rank), 
-              values_from = "Abundance",
-              values_fill = 0)%>%
-  as.data.frame()
-#' Column names are ASVs and row names are sample IDs.
-rownames(ps.q.df.wide.pca)<-ps.q.df.wide.pca$Sample
-#' Remove the Sample column (the first column)
-ps.q.df.wide.pca<-ps.q.df.wide.pca[,-1] 
-ps.q.df.wide.pca<-as.matrix(ps.q.df.wide.pca)
-#' The first crucial step is to transform the compositional data:
-#' Use a log-ratio transformation, typically the centered log-ratio 
-#' (clr) or isometric log-ratio (ilr) transformation.
-#' The clr transformation is often preferred for interpretability, 
-#' while ilr is used for statistical properties.
-#' We will use robust CLR (RCLR) with natural log.
-ps.q.df.wide.pca.tfm<-decostand(ps.q.df.wide.pca,
-                                method="rclr",
-                                logbase = exp)
-#' Calculate principal components
-pca.q<-prcomp(ps.q.df.wide.pca.tfm)
-str(pca.q)
+# ps.q.df.pca.input <- readRDS(
+#   file.path(community.composition.rdafiles,
+#             paste0(
+#     paste("ps.q.df.rare-nonfiltered",agglom.rank,
+#       paste(custom.levels,collapse = '-'),sep = "-"),
+#     ".rds")))
+#' #' Convert into wide format.
+#' ps.q.df.wide.pca<-ps.q.df.pca.input%>%
+#'   dplyr::select(all_of(c(agglom.rank,"Sample","Abundance")))%>%
+#'   pivot_wider(names_from = all_of(agglom.rank), 
+#'               values_from = "Abundance",
+#'               values_fill = 0)%>%
+#'   as.data.frame()
+#' #' Column names are ASVs and row names are sample IDs.
+#' ps.q.df.wide.pca<-ps.q.df.wide.pca%>%
+#'   column_to_rownames("Sample")%>%
+#'   as.matrix()
+#' #' The first crucial step is to transform the compositional data:
+#' #' Use a log-ratio transformation, typically the centered log-ratio 
+#' #' (clr) or isometric log-ratio (ilr) transformation.
+#' #' The clr transformation is often preferred for interpretability, 
+#' #' while ilr is used for statistical properties.
+#' #' We will use robust CLR (RCLR) with natural log.
+#' ps.q.df.wide.pca.tfm<-decostand(ps.q.df.wide.pca,
+#'                                 method="rclr",
+#'                                 logbase = exp(1))
+#' #' Calculate principal components
+#' pca.q<-prcomp(ps.q.df.wide.pca.tfm)
+#' str(pca.q)
 #' * `sdev` is standard deviations of principal components. Used to 
 #' construct the scree plot   
 #' * `rotation` is loadings  
@@ -114,7 +165,8 @@ pca.q$sdev^2 / sum(pca.q$sdev^2)
 #' Calculate total variance explained by each principal component. 
 var_explained = pca.q$sdev^2 / sum(pca.q$sdev^2)
 #' Create a scree plot:
-qplot(seq_along(1:nrow(ps.q.df.wide.pca.tfm)), var_explained) + 
+# qplot(seq_along(1:nrow(ps.q.df.wide.pca.tfm)), var_explained) + 
+qplot(seq_along(1:nrow(rclr_avg)), var_explained) + 
   geom_line() + 
   xlab("Principal Component") + 
   ylab("Variance Explained") +
@@ -143,9 +195,11 @@ perc.var<-round(100*summary(pca.q)$importance[2,1:2],2)
 perc.var
 #' Combine the principal component vectors into a dataframe:
 PC1.df <- as.data.frame(PC1)%>%
-  mutate(Sample = rownames(ps.q.df.wide.pca))
+  # mutate(Sample = rownames(ps.q.df.wide.pca))
+  mutate(Sample = rownames(rclr_avg))
 PC2.df <- as.data.frame(PC2)%>%
-  mutate(Sample = rownames(ps.q.df.wide.pca))
+  # mutate(Sample = rownames(ps.q.df.wide.pca))
+  mutate(Sample = rownames(rclr_avg))
 #' Plot the PCA results:
 pca.plot<-PC1.df%>%
   left_join(PC2.df)%>%
@@ -248,19 +302,24 @@ loadings.df<-pc1.loadings%>%
   mutate(PC_num=ifelse(n==2,"PC1_and_PC2",PC_num))%>%
   distinct(Genus,PC_num)
 loadings.df  
-#' Check if the loadings are found in the data:
-source(file.path(util.functions.r,"add_relab_to_tax_df.R"))
-ps.q.agg.pc_loadings<-add_relab_to_tax_df(ps.q.agg,"Genus")%>%
-  distinct(class,Genus,MeanRelativeAbundance,sdRelativeAbundance)%>%
-  right_join(loadings.df)
-# write.table(ps.q.agg.pc_loadings,
-#             file=file.path(rtables.directory,
-#                            paste(paste(format(Sys.time(),format="%Y%m%d"),
-#                                        format(Sys.time(),format = "%H_%M_%S"),sep = "_"),
-#                                  "pca-loadings-mean_relative_abundance.tsv",sep="-")),
-#             row.names = F,sep = "\t")
-ps.q.agg.pc_loadings
+#' Check mean relative abundance of loadings.
+mean_sd_relab.all_hosts<- read.table(file.path(community.composition.tables,
+                                               paste0("mean-max-min-sd-relab-all_hosts-",agglom.rank,".tsv")),
+                                     sep = "\t", header = T)
+
+ps.q.agg.pc_loadings<-loadings.df %>%
+  left_join(mean_sd_relab.all_hosts, by ="Genus")
+ps.q.agg.pc_loadings%>%
+  knitr::kable(format = "simple")
+
+ps.q.agg.pc_loadings.fname <-file.path(diversity.tables,
+                                       paste("pca-loadings-mean_relative_abundance.tsv",sep="-"))
+if(!file.exists(ps.q.agg.pc_loadings.fname)){
+  write.table(ps.q.agg.pc_loadings,
+              file = ps.q.agg.pc_loadings.fname,
+              row.names = F,sep = "\t")
+}
 
 sessionInfo()
-rm(list =setdiff(ls(all.names = TRUE), "active.analysis"))
+rm(list =setdiff(ls(all.names = TRUE), c("active.analysis","markdown.dir")))
 gc()
